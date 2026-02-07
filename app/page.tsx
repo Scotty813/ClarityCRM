0da35e4a0924e6e50db2c1d091fd51913164d025
Scott Horn <shorn813@outlook.com>
2026-02-07 16:03:23 -0500

    added button component and setup tests

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -36,12 +37,9 @@ export default function Home() {
             center.
           </p>
         </div>
-        <button
-            onClick={() => alert("Hello from Clarity CRM!")}
-            className="flex h-12 items-center justify-center rounded-full bg-foreground px-6 text-background font-medium transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc]"
-          >
+        <Button onClick={() => alert("Hello from Clarity CRM!")}>
             Hello
-          </button>
+          </Button>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
